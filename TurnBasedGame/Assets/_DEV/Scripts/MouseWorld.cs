a33fdd1f83849451bdb20b405feed5c9b1ed6e56
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-10-29 20:55:47 +0000

    turn based game unity movement

---
+++ TurnBasedGame/Assets/_DEV/Scripts/MouseWorld.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    // Singleton instance to allow easy access to the MouseWorld class
+    private static MouseWorld instance;
+
+    // Layer mask to specify which layer to use for raycasting the mouse position
+    private LayerMask mousePlaneLM;
+
+    private void Awake()
+    {
+        // Set the instance to this MouseWorld object on initialization
+        instance = this;
+    }
+
+    // Static method to get the world position of the mouse cursor
+    public static Vector3 GetMousePosition()
+    {
+        // Cast a ray from the main camera through the mouse position on the screen
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Raycast against the specified layer to find the point on the "mouse plane"
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLM);
+
+        // Return the world position where the ray intersects the mouse plane
+        return raycastHit.point;
+    }
+}
---
+++ TurnBasedGame/Assets/_DEV/Scripts/Unit.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Unit : MonoBehaviour
+{
+    // Speed at which the unit moves
+    private float moveSpeed = 3f;
+    // Target position the unit will move towards
+    private Vector3 targetPos;
+
+    private void Update()
+    {
+        // Define a minimum stopping distance from the target position
+        float stoppingDistance = 0.01f;
+
+        // Check if the unit is farther from the target than the stopping distance
+        if (Vector3.Distance(transform.position, targetPos) > stoppingDistance)
+        {
+            // Calculate the direction towards the target position and normalize it
+            Vector3 moveDirection = (targetPos - transform.position).normalized;
+
+            // Move the unit towards the target based on the direction, speed, and deltaTime
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+            
+        // Check for a left mouse button click to set a new target position
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Call the Move function with the mouse world position as the new target position
+            Move(MouseWorld.GetMousePosition());
+            
+        }
+    }
+
+    // Function to set the target position for the unit to move towards
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPos = targetPosition;
+    }
+}
